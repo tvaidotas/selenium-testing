5fe2ab7e96ffba5487efe564ea4a5537de040a00
tvaidotas <tadas.vaidotas@gmail.com>
2020-04-28 16:17:07 +0100

    added a test to search for doggies on google using selenium

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -2,8 +2,14 @@ package org.example;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static java.lang.Thread.*;
+import static org.junit.Assert.assertTrue;
 
 public class AppTest
 {
@@ -13,6 +19,23 @@ public class AppTest
     @Before
     public void setUp(){
         driver = new ChromeDriver();
+    }
+
+    @Test
+    public void seleniumExampleTest() throws InterruptedException{
+        driver.manage().window().maximize();
+        sleep(2000);
+        driver.get("http://www.google.com");
+        sleep(1000);
+        WebElement googleSearchBar = driver.findElement(By.name("q"));
+        googleSearchBar.sendKeys("funny dog pics");
+        sleep(1000);
+        googleSearchBar.submit();
+        WebElement linkToPictures = driver.findElement(By.partialLinkText("Images for funny dog"));
+        linkToPictures.click();
+        sleep(1000);
+        WebElement imagesLink = driver.findElement(By.className("NZmxZe"));
+        assertTrue(imagesLink.isDisplayed());
     }
 
     @After
