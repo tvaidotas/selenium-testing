ba10e326b33e7ed966f1d593eab703dde5353ace
tvaidotas <tadas.vaidotas@gmail.com>
2020-04-29 13:43:26 +0100

    added the three types of wait examples

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,19 +1,24 @@
 package org.example;
 
+import com.google.common.base.Function;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import javax.swing.*;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
 
 import static java.lang.Thread.*;
+import static java.util.concurrent.TimeUnit.*;
 import static org.junit.Assert.assertTrue;
 
 public class AppTest
@@ -87,6 +92,40 @@ public class AppTest
         Actions action = new Actions(driver);
         action.moveByOffset(750, 450).clickAndHold().moveByOffset(0, 50).release().perform();
         sleep(5000);
+    }
+
+    @Test
+    public void implicitWaitExample(){
+        // bad way of doing it
+        driver.manage().timeouts().implicitlyWait(10, SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://www.google.com");
+        WebElement searchBar = driver.findElement(By.name("q"));
+        assertTrue(searchBar.isDisplayed());
+    }
+
+    @Test
+    public void explicitWaitExample(){
+        driver.get("http://www.google.com");
+        WebElement searchBar = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.name("q")));
+        assertTrue(searchBar.isDisplayed());
+    }
+
+    @Test
+    public void fluentWaitExample(){
+        driver.get("http://www.google.com");
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                .withTimeout(30, SECONDS)
+                .pollingEvery(5, SECONDS)
+                .ignoring(NoSuchElementException.class);
+        WebElement searchBar = wait.until(new Function<WebDriver, WebElement>() {
+            @Override
+            public WebElement apply(WebDriver webDriver) {
+                return driver.findElement(By.name("q"));
+            }
+        });
+        assertTrue(searchBar.isDisplayed());
     }
 
     @After
