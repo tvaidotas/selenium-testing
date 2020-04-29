f4bd8bd10677889ca07d3661eaad1848bd18fc78
tvaidotas <tadas.vaidotas@gmail.com>
2020-04-29 09:53:01 +0100

    adding a few more test to show how to send keyboard keys to the elements

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -31,6 +32,39 @@ public class AppTest
         googleSearchBar.sendKeys("funny dog pics");
         sleep(1000);
         googleSearchBar.submit();
+        WebElement linkToPictures = driver.findElement(By.partialLinkText("Images for funny dog"));
+        linkToPictures.click();
+        sleep(1000);
+        WebElement imagesLink = driver.findElement(By.className("NZmxZe"));
+        assertTrue(imagesLink.isDisplayed());
+    }
+
+    @Test
+    public void seleniumExampleSendingKeyboardKeysTest() throws InterruptedException{
+        driver.manage().window().maximize();
+        sleep(2000);
+        driver.get("http://www.google.com");
+        sleep(1000);
+        WebElement googleSearchBar = driver.findElement(By.name("q"));
+        googleSearchBar.sendKeys("funny dog pics");
+        sleep(1000);
+        googleSearchBar.sendKeys(Keys.ENTER);
+        WebElement linkToPictures = driver.findElement(By.partialLinkText("Images for funny dog"));
+        linkToPictures.click();
+        sleep(1000);
+        WebElement imagesLink = driver.findElement(By.className("NZmxZe"));
+        assertTrue(imagesLink.isDisplayed());
+    }
+
+    @Test
+    public void seleniumExampleSendingMultipleKeyboardKeysTest() throws InterruptedException{
+        driver.manage().window().maximize();
+        sleep(2000);
+        driver.get("http://www.google.com");
+        sleep(1000);
+        WebElement googleSearchBar = driver.findElement(By.name("q"));
+        googleSearchBar.sendKeys(Keys.chord("funny ", "dog ", "pics", Keys.ENTER));
+        sleep(1000);
         WebElement linkToPictures = driver.findElement(By.partialLinkText("Images for funny dog"));
         linkToPictures.click();
         sleep(1000);
