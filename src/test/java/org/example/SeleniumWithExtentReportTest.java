df4dd9ab1144f7ced1ee247297f86d982a0bc7b4
tvaidotas <tadas.vaidotas@gmail.com>
2020-04-30 10:43:42 +0100

    added a after test method to specify whether the test failed or passed and close the report with chrome driver
    added an example test for checking QA websites title

--- src/test/java/org/example/SeleniumWithExtentReportTest.java
+++ src/test/java/org/example/SeleniumWithExtentReportTest.java
@@ -2,13 +2,17 @@ package org.example;
 
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
 
 import java.io.File;
+
+import static org.testng.Assert.assertEquals;
 
 public class SeleniumWithExtentReportTest {
 
@@ -19,18 +23,41 @@ public class SeleniumWithExtentReportTest {
     @BeforeTest
     public void startReport(){
         report = new ExtentReports(
-                System.getProperty("user.dir" + "/test-output/Report.html"),
+                System.getProperty("user.dir") + "/test-output/Report.html",
                 true
         );
         report
                 .addSystemInfo("Host Name", "QA")
                 .addSystemInfo("Tester", "Tadas");
-        report.loadConfig(new File(System.getProperty("user.dir" + "\\extent-report.xml")));
+        report.loadConfig(new File(System.getProperty("user.dir") + "\\extent-report.xml"));
     }
 
     @BeforeMethod
     public void setUp(){
         driver = new ChromeDriver();
+    }
+
+    @Test
+    public void testQATitle(){
+        test = report.startTest("Verifying the title of QA website");
+        driver.manage().window().maximize();
+        test.log(LogStatus.INFO, "Started chrome browser and made it fullscreen");
+        driver.get("https://www.qa.com");
+        test.log(LogStatus.INFO, "Navigating to the QA website");
+        assertEquals(driver.getTitle(), "Virtual and online classes in technology, project management and leadership | QA");
+        test.log(LogStatus.PASS, "The title was exactly the same");
+    }
+
+    @AfterMethod
+    public void getResult(ITestResult result){
+        driver.close();
+        if(result.getStatus() == ITestResult.FAILURE){
+            test.log(LogStatus.FAIL, "Test has failed " + result.getName());
+            test.log(LogStatus.FAIL, "Test has failed " + result.getThrowable());
+        } else if (result.getStatus() == ITestResult.SUCCESS) {
+            test.log(LogStatus.PASS, "Test has passed " + result.getName());
+        }
+        report.endTest(test);
     }
 
     @AfterTest
